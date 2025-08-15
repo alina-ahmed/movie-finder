09fe2cc69f3ae06a9cf5c0943fe3b076688f6901
Alina Ahmed Qureshi <71174202+alina-ahmed@users.noreply.github.com>
2025-08-15 23:39:29 +0500

    Added scroll to top on navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import MovieDetails from './MovieDetails.jsx'
 import AllMovies from './AllMovies.jsx'
 import { StyledEngineProvider } from '@mui/material/styles';
+import Layout from './Layout.jsx'
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <>
-  <Route path="/" element={<App />} />
-  <Route path='/details/:id' element={<MovieDetails />} />
-  <Route path='/movies-list' element={<AllMovies />} />
-</>
 
-))
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        children: [
+          { index: true, element: <App /> },
+          { path: 'details/:id', element: <MovieDetails /> },
+          { path: 'movies-list', element: <AllMovies /> }
+        ]
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
